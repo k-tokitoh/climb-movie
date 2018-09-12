6f4cc40364419b051ea8652241fabdc88b7197eb
k-tokitoh <tsuzuki670@gmail.com>
2018-09-12 15:19:24 +0000

    seedファイルに管理者ユーザのパスワードが漏れていたので追加した

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'admin', email:'admin@climbmania.jp', admin: true)
+User.create(name: 'admin', email:'admin@climbmania.jp', password: 'yamashitayuji', admin: true)
 
 Region.create(name: '北海道')
 Region.create(name: '東北')
