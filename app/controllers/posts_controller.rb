3d20cb793577fba83db339e955ef5c50934cf946
kamohara.jun <23qkamohalalalajun@gmail.com>
2018-09-15 14:12:50 +0000

    検索機能初期実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,8 +36,21 @@ class PostsController < ApplicationController
     end
     
     def search
-        @posts = Post.where(id: params[:q])
+        matched_ids = Post.pluck(:id).select{ |i|       # ポストの各idに対し、
+            words = get_words(post_id: i)               # まずそれにひもづく単語リストwordsを取得
+
+            params[:q].split().map{ |qword|             # クエリをsplitして得られた各単語qwordに対して
+                words.any?{ |word|                      # そのqwordが単語リストwordsに
+                    word.include?(qword)                # 部分文字列として登場するか
+                }                                       # 1回でも登場したらtrueを返す
+            }.all?                                      # 全てのqwordが条件を満たすidを取ってくる
+            
+        }
         #byebug
+        logger.debug('prams')
+        logger.debug(params[:q])
+        logger.debug(matched_ids)
+        @posts = Post.where(id: matched_ids).page(params[:page]).per(10)              # 選択されたidのみ表示する
         render 'index'
     end
     
@@ -45,6 +58,18 @@ class PostsController < ApplicationController
 
     def post_params
       params.require(:post).permit(:approved)
+    end
+    
+    def get_words(post_id:)
+        ps = Problem.includes(:posts).where('posts.id'=>post_id)    # idがpost_idのpostに紐づく課題たち
+        psnames = ps.pluck(:name, :grade).flatten.uniq              # 課題名と級を格納した配列
+        rocknames = ps.pluck(:rock_id).uniq.map{ |rock_id|          # 各課題の岩idに対し(重複をのぞいてから)
+            rock = Rock.find(rock_id)                               # 岩を取得
+            area = Area.find(rock.area_id)                          # エリア取得
+            region = Region.find(area.region_id)                    # 地方取得
+            [rock.name, area.name, region.name]                     # 岩、エリア、地方の名前
+        }.flatten.uniq               # 複数の岩に渡るpostではここで、さらに連結される(実際はあまりないか?)
+        psnames + rocknames          # 連結された課題名と、岩名をくっつける
     end
     
 end
