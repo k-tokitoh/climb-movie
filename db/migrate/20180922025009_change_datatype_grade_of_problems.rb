dcc58473743bc06fd31ec37850ed8645b4f0804a
k-tokitoh <tsuzuki670@gmail.com>
2018-09-24 15:00:12 +0000

    マイグレーションファイルに文言を追加

--- db/migrate/20180922025009_change_datatype_grade_of_problems.rb
+++ db/migrate/20180922025009_change_datatype_grade_of_problems.rb
@@ -1,5 +1,6 @@
 class ChangeDatatypeGradeOfProblems < ActiveRecord::Migration[5.0]
   def change
-    change_column :problems, :grade, :integer
+    # change_column :problems, :grade, :integer
+    change_column :problems, :grade, 'integer USING CAST(grade AS integer)'
   end
 end
