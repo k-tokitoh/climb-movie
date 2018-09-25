f09a7afc641919f0376bb1cec4b1e94cbfe19aa3
k-tokitoh <tsuzuki670@gmail.com>
2018-09-25 12:49:08 +0000

    gradeのバリデーションを修正した

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,5 +2,5 @@ class Problem < ApplicationRecord
   belongs_to :rock
   has_and_belongs_to_many :posts
   validates :name, uniqueness: true, presence:true
-  validates :grade, inclusion: { in: %w(10級 9級 8級 7級 6級 5級 4級 3級 2級 1級 初段 二段 三段 四段 五段 六段) }
+  validates :grade, inclusion: { in: 1..16 }
 end
