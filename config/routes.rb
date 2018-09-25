7b82f97c97f942e1681ba62296efc6bda7cb54f9
k-tokitoh <tsuzuki670@gmail.com>
2018-09-25 15:07:56 +0000

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'youtubeAPI', to: 'posts#get_youtube_videos'
   get 'search', to: 'posts#search'                      # 検索
   
+  get 'admin', to: 'admin#index'
+
   get '/area/:id', to: 'areas#show'
   
   post '/set_refine_search', to: 'posts#set_refine_search'
