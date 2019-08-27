56fd8e935bb769cb2b2230df0d5ae01bfaec6519
takashi suzuki <takashi@takashinoMacBook-puro.local>
2019-08-28 00:40:37 +0900

    DEV-0001 インデントなど修正

--- lib/youtube/api.rb
+++ lib/youtube/api.rb
@@ -1,16 +1,16 @@
 class Youtube::Api
-	def initialize
-		@api = Google::Apis::YoutubeV3::YouTubeService.new
-		@api.key = ENV["YOUTUBE_API_KEY"]
-	end
+  def initialize
+    @api = Google::Apis::YoutubeV3::YouTubeService.new
+    @api.key = ENV["YOUTUBE_API_KEY"]
+  end
 
-	def search(problem)
-		@api.list_searches(
-			"id, snippet",				#取得内容
-			type: "video",				#チャンネルやプレイリストを含まず、動画のみ取得する
-			q: problem.name,			#検索キーワードの指定（検索方法は他にも色々あり）
-			max_results: 10				#最大取得件数
-		).items									#動画リソースのみにフィルター
-		.map {|api_response_video| Youtube::Video.new(api_response_video)}
-	end
+  def search(problem)
+    @api.list_searches(
+      "id, snippet",				#取得内容
+      type: "video",				#チャンネルやプレイリストを含まず、動画のみ取得する
+      q: problem.name,			#検索キーワードの指定（検索方法は他にも色々あり）
+      max_results: 10				#最大取得件数
+    ).items									#動画リソースのみにフィルター
+    .map {|api_response_video| Youtube::Video.new(api_response_video)}
+  end
 end
--- lib/youtube/video.rb
+++ lib/youtube/video.rb
@@ -1,42 +1,42 @@
 class Youtube::Video
-	attr_reader :title, :snippet, :id
+  attr_reader :title, :snippet, :id
 
-	def initialize(api_response_video)
-		@title = trim(api_response_video.snippet.title)
-		@snippet = @title + trim(api_response_video.snippet.description)
-		@id = api_response_video.id.video_id
-	end
+  def initialize(api_response_video)
+    @title = trim(api_response_video.snippet.title)
+    @snippet = @title + trim(api_response_video.snippet.description)
+    @id = api_response_video.id.video_id
+  end
 
-	private
+  private
 
-	# youtubeの動画情報及びDB側の情報を整形し、照合可能にする
-	def trim(text)
-		return text if text.nil?
+  # youtubeの動画情報及びDB側の情報を整形し、照合可能にする
+  def trim(text)
+    return text if text.nil?
 
-		# 絵文字などの4バイト文字を取り除く（mysqlが絵文字に対応していないため）
-		text.each_char do |b|
-			text.delete!(b) if b.bytesize == 4
-		end
+    # 絵文字などの4バイト文字を取り除く（mysqlが絵文字に対応していないため）
+    text.each_char do |b|
+      text.delete!(b) if b.bytesize == 4
+    end
 
-		# 大文字を小文字にする
-		text.downcase!
+    # 大文字を小文字にする
+    text.downcase!
 
-		# 全角英数字を半角にする
-		text.tr!('０-９ａ-ｚ', '0-9a-z')
+    # 全角英数字を半角にする
+    text.tr!('０-９ａ-ｚ', '0-9a-z')
 
-		# "・"を取り除く
-		text.tr!("・", "")
+    # "・"を取り除く
+    text.tr!("・", "")
 
-		# 半角及び全角のスペースを取り除く
-		text.gsub!(/(\s|　)+/, '')
+    # 半角及び全角のスペースを取り除く
+    text.gsub!(/(\s|　)+/, '')
 
-		# グレード表記を統一する
-		[GRADE_QDver, GRADE_chara].each do |hash|
-			hash.each do |grade_int, grade_string|
-				text.gsub!(/#{grade_string}/, GRADE_CORRESPONDENCE.to_h.invert[grade_int])
-			end
-		end
+    # グレード表記を統一する
+    [GRADE_QDver, GRADE_chara].each do |hash|
+      hash.each do |grade_int, grade_string|
+        text.gsub!(/#{grade_string}/, GRADE_CORRESPONDENCE.to_h.invert[grade_int])
+      end
+    end
 
-		return text
-	end
+    return text
+  end
 end
