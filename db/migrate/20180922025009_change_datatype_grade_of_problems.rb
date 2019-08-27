b6834df5c73f7422e7f6c9c1dd5be9bb112fecf8
takashi suzuki <takashi@takashinoMacBook-puro.local>
2019-08-27 21:49:58 +0900

    Revert "マイグレーションファイルに文言を追加"
    This reverts commit dcc58473743bc06fd31ec37850ed8645b4f0804a.

--- db/migrate/20180922025009_change_datatype_grade_of_problems.rb
+++ db/migrate/20180922025009_change_datatype_grade_of_problems.rb
@@ -1,6 +1,5 @@
 class ChangeDatatypeGradeOfProblems < ActiveRecord::Migration[5.0]
   def change
-    # change_column :problems, :grade, :integer
-    change_column :problems, :grade, 'integer USING CAST(grade AS integer)'
+    change_column :problems, :grade, :integer
   end
 end
