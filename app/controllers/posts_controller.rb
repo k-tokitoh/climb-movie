900a3bb25e189dca9d6daf5c9d7fea3c16bc59d4
takashi suzuki <takashi@takashinoMacBook-puro.local>
2019-08-28 10:09:00 +0900

    DEV-0001 controllerの処理をmodelに切り出し

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,45 +45,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    youtube = Youtube::Api.new
-
-    Problem.all.each do |problem|
-      videos = youtube.search(problem)
-
-      videos.each do |video|
-        matched_post = Post.find_by(video: video.id)
-        # 既にそのvideo_idを持っている場合、video_id以外の属性を更新する
-        if matched_post.present?
-          matched_post.update(title: video.title)
-        # まだそのvideo_idを持っていない場合、新たにpostを作成する
-        else
-          matched_post = problem.posts.create(video: video.id, title: video.title, approved: 'undecided', hit: 0)
-        end
-
-        # youtubeの動画情報が、紐づけようとしているエリアの名前（又はその別名）を含むか調べる
-        if problem.rock.area.other_names.present?
-          area_match = true if problem.rock.area.other_names.split(',').unshift(problem.rock.area.name).any?{|i|video.snippet.include?(i)}
-        else
-          area_match = true if video.snippet.include?(problem.rock.area.name)
-        end
-
-        # youtubeの動画情報が、紐づけようとしている課題の名前（又はその別名）を含むか調べる
-        if problem.other_names.present?
-          problem_name_match = true if problem.other_names.split(',').unshift(problem.name).any?{|i|video.snippet.include?(i)}
-        else
-          problem_name_match = true if video.snippet.include?(problem.name)
-        end
-
-        # youtubeの動画情報が、紐づけようとしている課題のグレードを含むか調べる（表記法はtrim()により既に統一してある）
-        problem_grade_match = true if video.snippet.include?(GRADE_CORRESPONDENCE.to_h.invert[problem.grade])
-
-        # youtubeの動画情報が、紐づけようとしているエリアの名前、課題の名前及びグレードを含む場合、その動画を直ちに公開する
-        if area_match && problem_name_match && problem_grade_match
-          matched_post.update(approved: 'OK')
-        end
-      end
-    end
-
+    Post.create_from_youtube
     redirect_to '/'
   end
 
